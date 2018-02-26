745994328f0d5f6f5d900a3423db83fd20ffe3b7
Olga Popova <olpop992@gmail.com>
2018-02-27 00:23:38 +0500

    Свёрстаны слайдер-услуги и партнеры

--- js/script.js
+++ js/script.js
@@ -1,55 +1,113 @@
 "use strict";
 
-var sliderControlsWrap = document.querySelector(".slider-controls");
-var sliderControls = sliderControlsWrap.querySelectorAll("button");
-var sliders = document.querySelectorAll(".slider-item");
+(function productSliderManage() {
+	var sliderControlsWrap = document.querySelector(".slider-controls");
+	var sliderControls = sliderControlsWrap.querySelectorAll("button");
+	var sliders = document.querySelectorAll(".slider-item");
 
-sliderControls[0].addEventListener("click", function() {
-	if (!sliderControls[0].classList.contains("active")) {
-		for (var i = 0; i < sliderControls.length; i++) {
+	sliderControls[0].addEventListener("click", function() {
+		if (!sliderControls[0].classList.contains("active")) {
+			for (var i = 0; i < sliderControls.length; i++) {
+					if (sliderControls[i].classList.contains("active")) {
+						sliderControls[i].classList.remove("active");
+						sliderControls[0].classList.add("active");
+
+						for (var j = 0; j < sliders.length; j++) {
+							sliders[j].classList.add("hidden");
+						}
+						sliders[0].classList.remove("hidden");
+						return;
+					}
+			}
+		}
+	})
+	sliderControls[1].addEventListener("click", function() {
+		if (!sliderControls[1].classList.contains("active")) {
+			for (var i = 0; i < sliderControls.length; i++) {
 				if (sliderControls[i].classList.contains("active")) {
 					sliderControls[i].classList.remove("active");
-					sliderControls[0].classList.add("active");
+					sliderControls[1].classList.add("active");
 
 					for (var j = 0; j < sliders.length; j++) {
-						sliders[j].classList.add("hidden");
-					}
-					sliders[0].classList.remove("hidden");
+							sliders[j].classList.add("hidden");
+						}
+						sliders[1].classList.remove("hidden");
 					return;
 				}
+			}
 		}
-	}
-})
-sliderControls[1].addEventListener("click", function() {
-	if (!sliderControls[1].classList.contains("active")) {
-		for (var i = 0; i < sliderControls.length; i++) {
-			if (sliderControls[i].classList.contains("active")) {
-				sliderControls[i].classList.remove("active");
-				sliderControls[1].classList.add("active");
+		
+	})
+	sliderControls[2].addEventListener("click", function() {
+		if (!sliderControls[2].classList.contains("active")) {
+			for (var i = 0; i < sliderControls.length; i++) {
+				if (sliderControls[i].classList.contains("active")) {
+					sliderControls[i].classList.remove("active");
+					sliderControls[2].classList.add("active");
+					for (var j = 0; j < sliders.length; j++) {
+							sliders[j].classList.add("hidden");
+						}
+						sliders[2].classList.remove("hidden");
+					return;
+				}
+			}
+		}
+	})
+})();
 
-				for (var j = 0; j < sliders.length; j++) {
-						sliders[j].classList.add("hidden");
+(function manageServicesSlider() {
+	var sliderWrap = document.querySelector(".services-slider-wrap");
+	var sliderControls = sliderWrap.querySelectorAll(".services-controls-item");
+	var sliders = sliderWrap.querySelectorAll(".services-item");
+
+	sliderControls[0].addEventListener("click", function() {
+		if (!sliderControls[0].classList.contains("active")) {
+			for (var i = 0; i < sliderControls.length; i++) {
+					if (sliderControls[i].classList.contains("active")) {
+						sliderControls[i].classList.remove("active");
+						sliderControls[0].classList.add("active");
+
+						for (var j = 0; j < sliders.length; j++) {
+							sliders[j].classList.add("hidden");
+						}
+						sliders[0].classList.remove("hidden");
+						return;
 					}
-					sliders[1].classList.remove("hidden");
-				return;
 			}
 		}
-	}
-	
-})
-sliderControls[2].addEventListener("click", function() {
-	if (!sliderControls[2].classList.contains("active")) {
-		for (var i = 0; i < sliderControls.length; i++) {
-			if (sliderControls[i].classList.contains("active")) {
-				sliderControls[i].classList.remove("active");
-				sliderControls[2].classList.add("active");
-				for (var j = 0; j < sliders.length; j++) {
-						sliders[j].classList.add("hidden");
-					}
-					sliders[2].classList.remove("hidden");
-				return;
+	})
+	sliderControls[1].addEventListener("click", function() {
+		if (!sliderControls[1].classList.contains("active")) {
+			for (var i = 0; i < sliderControls.length; i++) {
+				if (sliderControls[i].classList.contains("active")) {
+					sliderControls[i].classList.remove("active");
+					sliderControls[1].classList.add("active");
+
+					for (var j = 0; j < sliders.length; j++) {
+							sliders[j].classList.add("hidden");
+						}
+						sliders[1].classList.remove("hidden");
+					return;
+				}
 			}
 		}
-	}
-	
-})
+		
+	})
+	sliderControls[2].addEventListener("click", function() {
+		if (!sliderControls[2].classList.contains("active")) {
+			for (var i = 0; i < sliderControls.length; i++) {
+				if (sliderControls[i].classList.contains("active")) {
+					sliderControls[i].classList.remove("active");
+					sliderControls[2].classList.add("active");
+					for (var j = 0; j < sliders.length; j++) {
+							sliders[j].classList.add("hidden");
+						}
+						sliders[2].classList.remove("hidden");
+					return;
+				}
+			}
+		}
+	})
+})();
+
+
